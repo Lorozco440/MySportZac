f7f32a550ee7095916adfea6e846b7557b9b8973
JQuiej <quiej008@gmail.com>
2024-02-29 19:33:57 -0600

    agregadas las funciones de login

--- app/src/main/java/com/umg/mysportzac/LoginActivity.kt
+++ app/src/main/java/com/umg/mysportzac/LoginActivity.kt
@@ -1,11 +1,225 @@
 package com.umg.mysportzac
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.core.widget.doOnTextChanged
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FacebookAuthProvider
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.firestore.FirebaseFirestore
+import java.text.SimpleDateFormat
+import java.util.Date
+import kotlin.properties.Delegates
 
 class LoginActivity : AppCompatActivity() {
+
+    companion object {
+        lateinit var usermail: String
+        lateinit var providerSession: String
+
+    }
+
+    private var email by Delegates.notNull<String>()
+    private var password by Delegates.notNull<String>()
+    private lateinit var etEmail: EditText
+    private lateinit var etPassword: EditText
+    private lateinit var lyTerms: LinearLayout
+
+    private lateinit var mAuth: FirebaseAuth
+    private  var RESULT_CODE_GOOGLE_SIGN_IN = 100
+    private val callbackManager = CallbackManager.Factory.create()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        lyTerms = findViewById(R.id.lyTerms)
+        lyTerms.visibility = View.INVISIBLE
+
+        //asignar variables a los elementos del activity
+        etEmail = findViewById(R.id.etEmail)
+        etPassword = findViewById(R.id.etPassword)
+        mAuth = FirebaseAuth.getInstance()
+
+        manageLogin()
+        etEmail.doOnTextChanged { text, start, before, count ->  manageLogin() }
+        etPassword.doOnTextChanged { text, start, before, count -> manageLogin()  }
+    }
+
+    public override fun onStart(){
+        super.onStart()
+        val currentUser = FirebaseAuth.getInstance().currentUser
+        if(currentUser != null) {
+            inicio(currentUser.email.toString(), currentUser.providerId)
+        }
+    }
+    /*override fun onBackPressed() {
+        val startMain = Intent(Intent.ACTION_MAIN)
+        startMain.addCategory(Intent.CATEGORY_HOME)
+        startMain.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        startActivity(startMain)
+    }*/
+    private fun manageLogin(){
+        var tvLogin = findViewById<TextView>(R.id.tvLogin)
+        email = etEmail.text.toString()
+        password = etPassword.text.toString()
+
+        if(TextUtils.isEmpty(password) || ValidateEmail.isEmail(email) == false){
+            tvLogin.setBackgroundColor(ContextCompat.getColor(this, R.color.gray))
+            tvLogin.isEnabled = false
+        }else{
+            tvLogin.setBackgroundColor(ContextCompat.getColor(this, R.color.green))
+            tvLogin.isEnabled = true
+        }
+    }
+    fun login(view: View) {
+        loginUser()
+    }
+
+    private fun loginUser(){
+        email = etEmail.text.toString()
+        password = etPassword.text.toString()
+
+        mAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    inicio(email, provider = "email")
+                } else {
+                    if (lyTerms.visibility == View.INVISIBLE) {
+                        lyTerms.visibility = View.VISIBLE
+                    } else {
+                        val cbAcept = findViewById<CheckBox>(R.id.cbAcept)
+                        if (cbAcept.isChecked) registrar()
+                    }
+                }
+            }
+    }
+
+    private fun inicio(email: String, provider: String){
+        usermail = email
+        providerSession = provider
+
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+    private fun registrar(){
+        email = etEmail.text.toString()
+        password = etPassword.text.toString()
+
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+                if(it.isSuccessful){
+                    val dateRegister = SimpleDateFormat("dd/MM/yyyy").format(Date())
+                    val dbRegister = FirebaseFirestore.getInstance()
+                    dbRegister.collection("users").document(email).set(hashMapOf(
+                            "user" to email,
+                            "dateRegister" to dateRegister
+                    ))
+
+                    inicio(email, "email")
+                }else{
+                    Toast.makeText(this, "Error al crear el Usuario", Toast.LENGTH_LONG).show()
+                }
+            }
+    }
+    fun goTerms(view: View){
+        startActivity(Intent(this, TermsActivity::class.java))
+    }
+    fun forgotPassword(view: View){
+        resetPassword()
+    }
+    private fun resetPassword(){
+        var e = etEmail.text.toString()
+        if (!TextUtils.isEmpty(e)){
+            mAuth.sendPasswordResetEmail(e)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) Toast.makeText(this, "Correo Enviado a $e", Toast.LENGTH_SHORT).show()
+                    else Toast.makeText(this, "No se encontró el usuario con este correo", Toast.LENGTH_SHORT).show()
+                }
+        }
+        else Toast.makeText(this, "Ingresa un Correo", Toast.LENGTH_SHORT).show()
+    }
+    fun callSignInFacebook(view: View){
+        signInFacebook()
+    }
+    private fun signInFacebook(){
+        LoginManager.getInstance().logInWithReadPermissions(this, listOf("email"))
+
+        LoginManager.getInstance().registerCallback(callbackManager, object :
+            FacebookCallback<LoginResult> {
+            override fun onSuccess(result: LoginResult) {
+                result.let{
+                    val token = it.accessToken
+                    val credential = FacebookAuthProvider.getCredential(token.token)
+                    mAuth.signInWithCredential(credential).addOnCompleteListener {
+                        if (it.isSuccessful){
+                            email = it.result.user?.email.toString()
+                            inicio(email, "Facebook")
+                        }
+                        else showError("Facebook")
+                    }
+                }
+                //handleFacebookAccessToken(loginResult.accessToken)
+            }
+
+            override fun onCancel() { }
+            override fun onError(error: FacebookException) { showError("Facebook") }
+        })
+    }
+    fun callSignInGoogle(view: View){
+        signInGoogle()
+    }
+    private fun signInGoogle(){
+        var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail().build()
+
+        var googleSignInClient = GoogleSignIn.getClient(this, gso)
+        googleSignInClient.signOut()
+        startActivityForResult(googleSignInClient.signInIntent, RESULT_CODE_GOOGLE_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
+        callbackManager.onActivityResult(requestCode, resultCode, data)
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RESULT_CODE_GOOGLE_SIGN_IN) {
+
+            try {
+                val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+                val account = task.getResult(ApiException::class.java)!!
+                if (account != null){
+                    email = account.email!!
+                    val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+                    mAuth.signInWithCredential(credential).addOnCompleteListener{
+                        if (it.isSuccessful) inicio(email, "Google")
+                        else Toast.makeText(this, "Error en la conexión con Google", Toast.LENGTH_SHORT)
+                    }
+                }
+            } catch (e: ApiException) {
+                Toast.makeText(this, "Error en la conexión con Google", Toast.LENGTH_SHORT)
+            }
+        }
+    }
+    private fun showError (provider: String){
+        Toast.makeText(this, "Error en la conexión con $provider", Toast.LENGTH_SHORT)
     }
 }
--- app/src/main/java/com/umg/mysportzac/MainActivity.kt
+++ app/src/main/java/com/umg/mysportzac/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.umg.mysportzac
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.facebook.login.LoginManager
+import com.google.firebase.auth.FirebaseAuth
+import com.umg.mysportzac.LoginActivity.Companion.usermail
+import com.umg.mysportzac.LoginActivity.Companion.providerSession
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Toast.makeText(this, "Hola $usermail", Toast.LENGTH_SHORT).show()
+    }
+    fun cerrarSesion(view: View) {
+     cerrar()
+    }
+    private fun cerrar(){
+        usermail = " "
+
+        if(providerSession == "Facebook") LoginManager.getInstance().logOut()
+        else
+        FirebaseAuth.getInstance().signOut()
+        startActivity(Intent(this, LoginActivity::class.java))
+
     }
 }

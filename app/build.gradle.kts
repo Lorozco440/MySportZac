47b02997756a3075fbf335c06fb801ead2b87e77
Lorozco440 <lorozcoc8@miumg.edu.gt>
2024-04-06 09:01:09 -0600

    Subidas Funciones de Uso de camara

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,6 +9,13 @@ android {
     namespace = "com.umg.mysportzac"
     compileSdk = 34
 
+   buildFeatures {
+       viewBinding = true
+   }
+    dataBinding {
+        enable = true
+    }
+
     defaultConfig {
         applicationId = "com.umg.mysportzac"
         minSdk = 24
@@ -45,6 +52,7 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.compose.foundation:foundation-layout-android:1.6.4")
     implementation("com.google.android.gms:play-services-maps:18.2.0")
+    implementation("androidx.activity:activity:1.8.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -66,4 +74,19 @@ dependencies {
     //seekBar circle, doc oficial https://github.com//tabkery/CircularSeekBar
     implementation ("me.tankery.lib:circularSeekBar:1.3.2")
     implementation ("com.google.android.gms:play-services-location:17.0.0")
+
+    //camara
+    implementation ("androidx.camera:camera-view:1.0.0-alpha23")
+    implementation("androidx.camera:camera-core:1.0.1")
+    implementation("androidx.camera:camera-camera2:1.0.1")
+// If you want to additionally use the CameraX Lifecycle library
+    implementation("androidx.camera:camera-lifecycle:1.0.1")
+// If you want to additionally use the CameraX View class
+    implementation("androidx.camera:camera-view:1.0.0-alpha27")
+// If you want to additionally use the CameraX Extensions library
+    implementation("androidx.camera:camera-extensions:1.0.0-alpha27")
+
+    implementation ("com.github.bumptech.glide:glide:4.12.0")
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
+
 }

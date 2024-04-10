0682d01250558ae99a30dfd660c2bd06ce790b4b
ManleoChacon03 <mchaconc8@miumg.edu.gt>
2024-04-10 01:47:57 -0600

    Historial

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -89,4 +89,7 @@ dependencies {
     implementation ("com.github.bumptech.glide:glide:4.12.0")
     annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
 
+    //RecyclerView
+    implementation ("androidx.recyclerview:recyclerview:1.2.0")
+
 }
